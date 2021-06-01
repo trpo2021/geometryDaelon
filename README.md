4640826adf93afd6d2811522224d1f64f1c516d0
Vladislav-Kuryachikh <kvc02@bk.ru.noreply.github.com>
2021-06-01 13:29:18 +0700

    fixes application

---
+++ README.md
@@ -0,0 +1,3 @@
+example of a valid request: circle(1 1,3)
+to run the test, enter the command " make test"
+to build the entire program, enter " make all"
---
+++ src/lib/lib_calc_trans.h
@@ -0,0 +1,11 @@
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#pragma once
+
+void calculation(double*);
+void translate(char*, double*);
+int figure_chek(double*, double*);
